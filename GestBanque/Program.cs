b4bb5967f4d392a1bb6fd9cd529320f09dc66215
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2020-06-05 14:39:33 +0200

    Ajout du modele Compte courant

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 
 namespace GestBanque
 {
@@ -7,6 +8,32 @@ namespace GestBanque
         static void Main(string[] args)
         {
             Console.WriteLine("Start Exo!");
+
+            Personne p1 = new Personne();
+            p1.Prenom = "Zaza";
+            p1.Nom = "Vanderquack";
+            p1.DateNaiss = new DateTime(2010, 01, 06);
+
+            Courant c1 = new Courant();
+            c1.Numero = "BE00001";
+            c1.Titulaire = p1;
+            c1.LigneDeCredit = 100;
+            c1.Depot(500);
+            AfficherCompte(c1);
+
+            c1.Retrait(250);
+            AfficherCompte(c1);
+
+            c1.Retrait(300);
+            c1.Retrait(300);
+            AfficherCompte(c1);
+
+        }
+
+        private static void AfficherCompte(Courant c)
+        {
+            Console.WriteLine($"{c.Numero} {c.Solde} - {c.Titulaire.NomComplet} ");
+            Console.WriteLine();
         }
     }
 }
---
+++ Models/Courant.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Courant
+    {
+        #region Champs
+        private double _LigneDeCredit;
+       // private double _Solde;
+        #endregion
+
+        #region Propriété
+        public string Numero { get; set; }
+        //public double Solde { get { return _Solde; }  } 
+        //↑ Prop sans setter mais avec le champs accessible
+
+        public double Solde { get; private set; }
+
+        public double LigneDeCredit
+        {
+            get { return _LigneDeCredit; }
+            set
+            {
+                if (value < 0)
+                {
+                    //TODO Generer une erreur
+                    return;
+                }
+
+                _LigneDeCredit = value;
+            }
+        }
+
+        public Personne Titulaire { get; set; }
+        #endregion
+
+        #region Methode
+        public void Retrait(double montant)
+        {
+            if(montant <= 0 )
+            {
+                // TODO Erreur Argument montant negatif
+                return;
+            }
+
+            if(Solde - montant < -LigneDeCredit)
+            {
+                // TODO Erreur Ligne de credit!!!
+                return;
+            }
+
+            Solde = Solde - montant;
+        }
+
+        public void Depot(double montant)
+        {
+            if (montant <= 0)
+            {
+                // TODO Erreur Argument montant negatif
+                return;
+            }
+
+            Solde = Solde + montant;
+        }
+        #endregion
+
+    }
+}
