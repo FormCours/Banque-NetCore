4dc242329210d0a33ab2d8bf18af4132499a60a4
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2020-06-05 12:26:52 +0200

    Création de la classe personne

---
+++ Models/Personne.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Models
+{
+    public class Personne
+    {
+        #region Champs
+        private string _Nom;
+        private string _Prenom;
+        private DateTime _DateNaiss;
+        #endregion
+
+        #region Prop
+        public string Nom
+        {
+            get
+            {
+                return _Nom;
+            }
+
+            set
+            {
+                _Nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return _Prenom;
+            }
+
+            set
+            {
+                _Prenom = value;
+            }
+        }
+
+        public DateTime DateNaiss
+        {
+            get
+            {
+                return _DateNaiss;
+            }
+
+            set
+            {
+                _DateNaiss = value;
+            }
+        }
+        #endregion
+
+    }
+}
